62477f764dcab36b573c16e6cfaf552eb8170218
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2022-11-27 13:28:09 +0100

    task 2 finished until step 2

---
+++ 4/mapreduce-wordcount/map.py
@@ -0,0 +1,25 @@
+import sys
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+import re
+
+# Setting up stemmer and lemmatizer
+stemmer = SnowballStemmer("english")
+lemmatizer = WordNetLemmatizer()
+
+# Mapping
+output = []
+# Skipping the header
+lines = iter(sys.stdin)
+next(lines)
+
+for line in lines:
+    fields = line.split(",")
+    text = re.sub(r'[\W]', ' ', fields[2])
+    words = text.split(" ")
+    words = list(filter(lambda a: a != '', words))
+
+    for word in words:
+        print(f'S,{stemmer.stem(word)},1')
+        print(f'L,{lemmatizer.lemmatize(word)},1') # TODO: Not working
+# print("rocks :", lemmatizer.lemmatize("rocks"))
---
+++ 4/mapreduce-wordcount/reduce.py
@@ -0,0 +1,24 @@
+import sys
+
+# Mapping
+prev_word = None
+prev_count = 0
+
+for line in sys.stdin:
+    line = line.strip()
+    token, word, count = line.split(',')
+
+    count = int(count)
+
+    if prev_word == word:
+        prev_count += count
+    else:
+        if prev_word:
+            print(f"{token},{prev_word},{prev_count}")
+        
+        prev_count = count
+        prev_word = word
+
+# Printing the last word
+if prev_word == word:
+    print(f"{token},{prev_word},{prev_count}")
